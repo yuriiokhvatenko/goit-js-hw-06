5c45b073430b326a2b5f36faf0acf084e8832ad9
yuriiokhvatenko <113927739+yuriiokhvatenko@users.noreply.github.com>
2023-02-18 18:00:27 +0200

    finish task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const newList = ingredients.map(ingredient => {
+  const newList = document.createElement("li");
+  newList.classList.add("item");
+  newList.textContent = ingredient;
+  return newList;
+});
+
+newList.forEach(elem => {
+  console.log(elem);
+});
+
+const linkToInsert = document.querySelector('#ingredients');
+linkToInsert.append(...newList);
