7700e6ffaef85285ad2db1ddd1103e21e85d4ca8
yuriiokhvatenko <113927739+yuriiokhvatenko@users.noreply.github.com>
2023-02-19 23:08:52 +0200

    fix

--- js/task-04.js
+++ js/task-04.js
@@ -1,18 +1,16 @@
 const btnDecrement = document.querySelector('[data-action="decrement"]');
 const btnIncrement = document.querySelector('[data-action="increment"]');
-// const spanValue = document.querySelector('#value');
+const spanValue = document.querySelector('#value');
 
 let counterValue = 0;
 
 const handleDecrementBtnClick = () => {
     counterValue -= 1;
-    ololo.textContent = counterValue;
-    return counterValue;
+    spanValue.textContent = counterValue;
 };
 const handleIncrementBtnClick = () => {
     counterValue += 1;
-    ololo.textContent = counterValue;
-    return counterValue;
+    spanValue.textContent = counterValue;
 };
 btnDecrement.addEventListener('click', handleDecrementBtnClick);
 btnIncrement.addEventListener('click', handleIncrementBtnClick);
