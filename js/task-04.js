e4db9ca181694b4301cf2f4d7b89b8a0159715a7
yuriiokhvatenko <113927739+yuriiokhvatenko@users.noreply.github.com>
2023-02-19 16:52:43 +0200

    fix naming

--- js/task-04.js
+++ js/task-04.js
@@ -1,19 +1,18 @@
 const btnDecrement = document.querySelector('[data-action="decrement"]');
 const btnIncrement = document.querySelector('[data-action="increment"]');
-const spanValue = document.querySelector('#value');
+// const spanValue = document.querySelector('#value');
 
 let counterValue = 0;
 
-const decrement = () => {
+const handleDecrementBtnClick = () => {
     counterValue -= 1;
-    spanValue.textContent = counterValue;
+    ololo.textContent = counterValue;
     return counterValue;
 };
-const increment = () => {
+const handleIncrementBtnClick = () => {
     counterValue += 1;
-    spanValue.textContent = counterValue;
+    ololo.textContent = counterValue;
     return counterValue;
 };
-
-btnDecrement.addEventListener('click', decrement);
-btnIncrement.addEventListener('click', increment);
+btnDecrement.addEventListener('click', handleDecrementBtnClick);
+btnIncrement.addEventListener('click', handleIncrementBtnClick);
