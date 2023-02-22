ac51a774227a19f1e9eafe8537f9c797317ed5d6
yuriiokhvatenko <113927739+yuriiokhvatenko@users.noreply.github.com>
2023-02-22 21:58:27 +0200

    task 9 v2

--- js/task-09.js
+++ js/task-09.js
@@ -2,12 +2,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
 const btn = document.querySelector('.change-color');
 const span = document.querySelector('.color');
-const makeBgColor = (color) => {
-  color = document.body.style.backgroundColor = getRandomHexColor(color);
-  span.textContent = `${color}`;
+const body = document.querySelector('body');
+
+const makeBgColor = () => {
+  const clr = getRandomHexColor();
+  body.style.backgroundColor = clr;
+  span.textContent = clr;
 }
 
 btn.addEventListener(`click`, makeBgColor);
